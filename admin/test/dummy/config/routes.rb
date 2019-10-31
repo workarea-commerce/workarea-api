fca2cb87904cef11da755653f5e28c8fe897aca1
Tom Scott <tscott@weblinc.com>
2019-10-31 14:26:51 -0400

    Update routes files

--- admin/test/dummy/config/routes.rb
+++ admin/test/dummy/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   mount Workarea::Core::Engine => '/'
   mount Workarea::Admin::Engine => '/admin', as: 'admin'
   mount Workarea::Storefront::Engine => '/', as: 'storefront'
-  mount Workarea::Api::Engine => '/api', as: 'api'
+  mount_workarea_api subdomain: 'api'
 end
