430d085d389371c94e3d5c14664927a748c40420
Ben Crouse <bcrouse@workarea.com>
2020-07-28 08:44:36 -0400

    Handle non-persisted recommendation settings
    This uses `find_or_initialize` to match our usage of
    `Recommendation::Settings` elsewhere in the platform.
    
    Fixes #19

--- admin/test/integration/workarea/api/admin/recommendation_settings_integration_test.rb
+++ admin/test/integration/workarea/api/admin/recommendation_settings_integration_test.rb
@@ -35,6 +35,20 @@ module Workarea
 
           assert_equal(['foo'], recommendation_settings.reload.product_ids)
         end
+
+        def test_unpersisted_recommendation_settings
+          get admin_api.product_recommendation_settings_path(@product.id)
+          result = JSON.parse(response.body)['recommendation_settings']
+          assert_equal(@product.id, result['_id'])
+          assert_equal(0, Recommendation::Settings.count)
+
+          patch admin_api.product_recommendation_settings_path(@product.id),
+            params: { recommendation_settings: { product_ids: ['foo'] } }
+
+          assert_equal(1, Recommendation::Settings.count)
+          assert_equal(@product.id, Recommendation::Settings.first.id)
+          assert_equal(['foo'], Recommendation::Settings.first.product_ids)
+        end
       end
     end
   end
