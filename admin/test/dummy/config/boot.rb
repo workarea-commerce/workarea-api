a7a2c2b595b87627b8979720f51ab7c8e41f10df
Jason Hill <jhill@macmini.tail999711.ts.net>
2026-02-05 16:29:54 -0500

    Require logger in dummy boot (ActiveSupport on Ruby 2.6)

--- admin/test/dummy/config/boot.rb
+++ admin/test/dummy/config/boot.rb
@@ -1,5 +1,7 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../../Gemfile', __dir__)
 
+require 'logger'
+
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 $LOAD_PATH.unshift File.expand_path('../../../lib', __dir__)
